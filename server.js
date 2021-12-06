1f919cfe9c080a5ece713634359f49f301ed780f
saumilkt <saumilkt@gmail.com>
2021-12-05 22:00:52 -0800

    adding db setup

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+
+class Database {
+  constructor(config) {
+      this.connection = mysql.createConnection(config);
+  }
+
+  query(sql, args) {
+      return new Promise((resolve, reject) => {
+          this.connection.query(sql, args, (err, rows) => {
+              if (err)
+                  return reject(err);
+              resolve(rows);
+          });
+      });
+  }
+
+  close() {
+      return new Promise((resolve, reject) => {
+          this.connection.end(err => {
+              if (err)
+                  return reject(err);
+              resolve();
+          });
+      });
+  }
+}
+
+const db = new Database({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "r1gBy1997",
+  database: "cmsDB"
+});
