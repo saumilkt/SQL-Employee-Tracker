4038997d71a72bf9f64aa8096770911a7c039540
saumilkt <saumilkt@gmail.com>
2021-12-05 22:02:28 -0800

    adding show functions

--- server.js
+++ server.js
@@ -76,3 +76,33 @@ function runApp() {
       }
   });
 }
+
+// Builds complete employee table
+async function showEmployeeSummary() {
+  console.log(' ');
+  await db.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      runApp();
+  });
+};
+
+// Builds a table which shows existing roles and their departments
+async function showRoleSummary() {
+  console.log(' ');
+  await db.query('SELECT r.id, title, salary, name AS department FROM role r LEFT JOIN department d ON department_id = d.id', (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      runApp();
+  })
+};
+
+// Builds a table which shows existing departments
+async function showDepartments() {
+  console.log(' ');
+  await db.query('SELECT id, name AS department FROM department', (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      runApp();
+  })
+};
